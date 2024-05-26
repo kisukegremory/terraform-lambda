117bfe3c56a9db52a9ae6f60356ad61bd046f226
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2024-05-26 10:55:38 -0300

    feat: added code for lambda

---
+++ src/lambda_vanilla/main.py
@@ -0,0 +1,18 @@
+import boto3
+
+session = boto3.Session()
+s3 = session.client("s3")
+
+def lambda_handler(event, context):
+    s3_obj = s3.get_object(
+        Bucket="terraform-lambda-vanilla-input-data",
+        Key="input-data.csv"
+    )
+
+    return {
+        'statusCode':200,
+        'lastModified': str(s3_obj['LastModified'])
+    }
+
+if __name__ == "__main__":
+    print(lambda_handler("batata","salada"))
